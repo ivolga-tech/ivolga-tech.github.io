1740074009085614016b929acc9cba727d6445fc
somakun <somabaka@gmail.com>
2020-06-30 15:53:05 +0300

    added assets folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,8 +9,8 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `assets`,
+        path: `${__dirname}/src/assets`,
       },
     },
     `gatsby-transformer-sharp`,
@@ -18,13 +18,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `iVolga Technologies - разработка веб-сервисов, мобильных приложений и игр.`,
+        short_name: `iVolga`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/img/icons/favicon-32x32.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-typescript`,

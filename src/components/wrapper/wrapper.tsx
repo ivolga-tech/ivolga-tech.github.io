3cf23bb95fd33bd37832371715a3c2e5bc82a8f4
somakun <somabaka@gmail.com>
2020-06-30 19:19:48 +0300

    added wrapper component and global styles

---
+++ src/components/wrapper/wrapper.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+import "./wrapper.css"
+
+type Props = {
+  children: React.ReactNode
+}
+
+const PageWrapper = ({ children }: Props) => (
+  <div className="page page--defaults" id="page">
+    <div className="page__wrapper">
+      <div className="page__img">
+        <div className="page__img-wrap">
+          <article className="icon icon--ivolga">
+            <img
+              src="../../assets/img/minified-svg/ivolga-bg.svg"
+              alt="ivolga-bg"
+              width="1083px"
+              height="1081px"
+            />
+          </article>
+        </div>
+      </div>
+
+      <div>{children}</div>
+    </div>
+  </div>
+)
+
+export default PageWrapper

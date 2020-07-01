050af93911b41869649ee63dff4a9588a58c9441
somakun <somabaka@gmail.com>
2020-07-01 18:28:28 +0300

    created portfolio page

---
+++ src/pages/portfolio.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "../components/layout/layout";
+import SEO from "../components/seo";
+
+const Portfolio = () => {
+
+    return (
+        <Layout>
+            <SEO title="portfolio" />
+            portfolio page
+        </Layout>
+    )
+}
+
+export default Portfolio

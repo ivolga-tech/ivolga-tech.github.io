ad351208ec647da0559efaa7137f33eadfdbc810
somakun <somabaka@gmail.com>
2020-07-01 18:53:57 +0300

    changed the header page code. Added menu and logo

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,36 +1,49 @@
-import { Link } from "gatsby"
 import React from "react"
+import "./header.css"
+import HeaderMenu from "../нeader-menu/нeader-menu"
+import { Link } from "gatsby"
 
-type Props = {
-  siteTitle: string
-}
+const Header = () => (
+  <header className="page__header">
+    <Link className="logo" to="/">
+      <img
+        className="icon icon--ivolga"
+        src="../../assets/img/minified-svg/logo-ivolga.svg"
+        alt="logo-ivolga"
+      />
+    </Link>
 
-const Header = ({ siteTitle }: Props) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+    {/*<div className="modal">*/}
+    {/*    <div className="modal__background"/>*/}
+    {/*    <div className="modal__form">*/}
+    {/*        <div className="card card--form card--modal">*/}
+    {/*            <form className="form form--contact-us" action="/" method="POST">*/}
+    {/*                <div className="form__title">Свяжитесь с нами</div>*/}
+    {/*                <div className="form__content">*/}
+    {/*                    <div className="form__group">*/}
+    {/*                        <label className="form__label">Как вас зовут ?</label>*/}
+    {/*                        <input id="modal-name" type="text" name="name" required />*/}
+    {/*                    </div>*/}
+    {/*                    <div className="form__group">*/}
+    {/*                        <label className="form__label">Телефон или email для связи</label>*/}
+    {/*                        <input id="modal-contact" type="text" name="contact" />*/}
+    {/*                    </div>*/}
+    {/*                    <div className="form__group">*/}
+    {/*                        <label className="form__label">Чем мы можем вам помочь ?</label>*/}
+    {/*                        <textarea id="modal-message" name="message" cols={30} rows={3} />*/}
+    {/*                    </div>*/}
+    {/*                </div>*/}
+    {/*                <div className="form__footer">*/}
+    {/*                    <button className="btn btn--lg btn--yellow" id="modal-send-message"*/}
+    {/*                            type="submit">Отправить*/}
+    {/*                    </button>*/}
+    {/*                </div>*/}
+    {/*            </form>*/}
+    {/*        </div>*/}
+    {/*    </div>*/}
+    {/*</div>*/}
+
+    <HeaderMenu />
   </header>
 )
 

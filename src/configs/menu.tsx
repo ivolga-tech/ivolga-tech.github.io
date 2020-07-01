4435bd0cbc129da8c34dc0c6804b0442674ccfa9
somakun <somabaka@gmail.com>
2020-07-01 18:55:46 +0300

    added a menu config

---
+++ src/configs/menu.tsx
@@ -0,0 +1,18 @@
+export const menu = [
+  {
+    url: "/",
+    name: "Главная",
+  },
+  {
+    url: "/portfolio/",
+    name: "Портфолио",
+  },
+  {
+    url: "/vocations/",
+    name: "Вакансии",
+  },
+  {
+    url: "/contacts/",
+    name: "Контакты",
+  },
+]

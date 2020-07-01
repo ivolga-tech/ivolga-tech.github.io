86275aba977a3e9b87af9c7ea238910efea5f6af
somakun <somabaka@gmail.com>
2020-07-01 18:28:09 +0300

    created vocations page

---
+++ src/pages/vocations.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "../components/layout/layout";
+import SEO from "../components/seo";
+
+const Vocations = () => {
+
+    return (
+        <Layout>
+            <SEO title="vocations" />
+            Vocations page
+        </Layout>
+    )
+}
+
+export default Vocations

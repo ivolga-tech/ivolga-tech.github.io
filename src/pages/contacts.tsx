bf84280593f3ec38dbc57062d6938a4afa19d216
somakun <somabaka@gmail.com>
2020-07-01 18:45:21 +0300

    created contacts page

---
+++ src/pages/contacts.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "../components/layout/layout";
+import SEO from "../components/seo";
+
+const Contacts = () => {
+
+    return (
+        <Layout>
+            <SEO title="contacts" />
+            Contacts page
+        </Layout>
+    )
+}
+
+export default Contacts

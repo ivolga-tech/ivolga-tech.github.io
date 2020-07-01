27492255b90e1e6346affd474b28d3824a25a53f
somakun <somabaka@gmail.com>
2020-07-01 18:58:44 +0300

    added menu component

---
+++ src/components/нeader-menu/нeader-menu.tsx
@@ -0,0 +1,58 @@
+import React from "react"
+import { Link } from "gatsby"
+import "./нeader-menu.css"
+import { menu } from "../../configs/menu"
+
+type EaderMenu = {
+  url: string
+  name: string
+}
+
+const HeaderMenu = () => {
+  const pathname: string =
+    typeof window !== "undefined" ? window.location.pathname : ""
+
+  return (
+    <div className="menu">
+      <ul className="list list--top-menu">
+        {menu.map((listItem: EaderMenu) => (
+          <li
+            className={
+              "list__item " + `${pathname === listItem.url ? "is-active" : ""}`
+            }
+            key={listItem.url}
+          >
+            <Link className="list__link" to={listItem.url}>
+              {listItem.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+
+      <div className="btn btn--yellow">
+        <a className="list__link" id="modal-open" href="#">
+          Связаться
+        </a>
+      </div>
+
+      {/*<a className="нeader-menu-btn нeader-menu-btn--open" href="#" id="нeader-menu-page-open">*/}
+      {/*    <div className="нeader-menu-btn__img">*/}
+      {/*        <svg className="icon icon--нeader-menu" width="18px" height="14px">*/}
+      {/*            <use xlink:href="#icon-нeader-menu"></use>*/}
+      {/*        </svg>*/}
+
+      {/*    </div>*/}
+      {/*</a>*/}
+      {/*<a className="нeader-menu-btn нeader-menu-btn--close" href="#" id="нeader-menu-page-close">*/}
+      {/*    <div className="нeader-menu-btn__img">*/}
+      {/*        <svg className="icon icon--close" width="14px" height="14px">*/}
+      {/*            <use xlink:href="#icon-close"></use>*/}
+      {/*        </svg>*/}
+
+      {/*    </div>*/}
+      {/*</a>*/}
+    </div>
+  )
+}
+
+export default HeaderMenu

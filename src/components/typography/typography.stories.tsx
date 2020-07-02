ebc52c21dbbad4c7fe98032bf211f260e1a538c0
somakun <somabaka@gmail.com>
2020-07-02 16:33:27 +0300

    added typography component

---
+++ src/components/typography/typography.stories.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Typography } from "./typography";
+
+export default { title: "Typography" };
+
+export const DefaultTypography = () => <Typography>text</Typography>;
+
+export const Title = () => <Typography component="h1">text</Typography>;
---
+++ src/components/typography/typography.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { joinCssClasses } from "../../utils/utils";
+
+type Props = {
+  children?: React.ReactNode;
+  className?: string;
+  component?: "p" | "h1" | "h2" | "span";
+};
+
+export const Typography = (props: Props) => {
+  const { children, component: Component = "p", className } = props;
+
+  return <Component className={joinCssClasses(Component, className)}>{children}</Component>;
+};

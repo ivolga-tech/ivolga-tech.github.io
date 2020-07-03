7adb3e5fded7ac51f7d2ac6490dd8ffc536f5ab5
somakun <somabaka@gmail.com>
2020-07-03 18:19:57 +0300

    added form component

---
+++ src/components/form/form.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./form.css";
+
+const Form = () => {
+  return (
+    <form className="form form--contact-us modal__form" action="/" method="POST">
+      <div className="form__title">Свяжитесь с нами</div>
+      <div className="form__content">
+        <div className="form__group">
+          <label className="form__label">Как вас зовут ?</label>
+          <input id="modal-name" type="text" name="name" required />
+        </div>
+        <div className="form__group">
+          <label className="form__label">Телефон или email для связи</label>
+          <input id="modal-contact" type="text" name="contact" />
+        </div>
+        <div className="form__group">
+          <label className="form__label">Чем мы можем вам помочь ?</label>
+          <textarea id="modal-message" name="message" cols={30} rows={3} />
+        </div>
+      </div>
+      <div className="form__footer">
+        <button className="btn btn--lg btn--yellow" id="modal-send-message" type="submit">
+          Отправить
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default Form;

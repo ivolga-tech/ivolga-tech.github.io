0faf3cc96f6663e41067847af0c179dfdb8602cf
somakun <somabaka@gmail.com>
2020-07-06 20:54:32 +0300

    added form type to card component

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -3,17 +3,19 @@ import "./card.css";
 
 type Props = {
   children: React.ReactNode;
-  cardType: "form-modal";
+  cardType: "form" | "form-modal";
   className?: string;
 };
 
 const Card = ({ children, cardType }: Props): JSX.Element => {
   const renderCardType = (): JSX.Element => {
     switch (true) {
+      case cardType === "form":
+        return <div className="card card--form">{children}</div>;
       case cardType === "form-modal":
         return <div className="card card--form card--modal">{children}</div>;
     }
-    return <div className="card card--form card--modal">{children}</div>;
+    return <div className="card">{children}</div>;
   };
 
   return renderCardType();

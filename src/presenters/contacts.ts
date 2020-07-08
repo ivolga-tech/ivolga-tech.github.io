c9dfd866d7c4309f899ff0d8212e6922ceb7153a
somakun <somabaka@gmail.com>
2020-07-08 14:02:14 +0300

    added presenters for contacts

---
+++ src/presenters/contacts.ts
@@ -0,0 +1,68 @@
+type ListItem = {
+  list_item_name: {
+    text: string;
+  };
+  list_item_content: {
+    text: string;
+  };
+};
+
+type SocialLink = {
+  social_links: {
+    url: string;
+  };
+  social_logo: {
+    url: string;
+  };
+};
+
+type Contacts = {
+  title: {
+    text: string;
+  };
+  subtitle: {
+    text: string;
+  };
+  content: ListItem[];
+  list_social_links: SocialLink[];
+};
+
+type ContactsItem = {
+  name: string;
+  content: string;
+};
+
+type SocialLinksItem = {
+  logo: string;
+  link: string;
+};
+
+export type ContactsPageVM = {
+  title: string;
+  subtitle: string;
+  contacts: ContactsItem[];
+  socialLinks: SocialLinksItem[];
+};
+
+export const toContactsPageVM = (contacts: Contacts): ContactsPageVM => {
+  const { title, content, list_social_links, subtitle } = contacts;
+
+  return {
+    title: title.text,
+    subtitle: subtitle.text,
+    contacts: content.map(contact => {
+      const { list_item_name, list_item_content } = contact;
+      return {
+        name: list_item_name.text,
+        content: list_item_content.text,
+      };
+    }),
+    socialLinks: list_social_links.map(socialLink => {
+      const { social_links, social_logo } = socialLink;
+      return {
+        logo: social_logo.url,
+        link: social_links.url,
+      };
+    }),
+  };
+};

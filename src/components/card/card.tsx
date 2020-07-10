64d1f96f5e5b06837eac862e188e67725a6f7ad0
somakun <somabaka@gmail.com>
2020-07-10 19:18:42 +0300

    added link card type

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,19 +1,28 @@
 import React from "react";
 import "./card.css";
+import { Link } from "gatsby";
 
 type Props = {
   children: React.ReactNode;
-  cardType: "form" | "form-modal";
+  cardType: "form" | "form-modal" | "link";
   className?: string;
+  to?: string;
+  title?: string;
 };
 
-const Card = ({ children, cardType }: Props): JSX.Element => {
+const Card = ({ children, cardType, className, to, title }: Props): JSX.Element => {
   const renderCardType = (): JSX.Element => {
     switch (true) {
       case cardType === "form":
         return <div className="card card--form">{children}</div>;
       case cardType === "form-modal":
         return <div className="card card--form card--modal">{children}</div>;
+      case cardType === "link":
+        return (
+          <Link to={to ? to : ""} className={className} data-toggle="link" title={title}>
+            {children}
+          </Link>
+        );
     }
     return <div className="card">{children}</div>;
   };

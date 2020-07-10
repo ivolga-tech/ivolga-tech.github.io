26a9d0522876305f8fc455eceba90d4033ef0cf9
somakun <somabaka@gmail.com>
2020-07-10 20:38:43 +0300

    added product page creator function

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,33 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const path = require("path");
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+
+  const pages = await graphql(`
+    {
+      products: allPrismicProductPage {
+        edges {
+          node {
+            uid
+            id
+          }
+        }
+      }
+    }
+  `);
+
+  const component = path.resolve("src/components/product-page/product-page.tsx");
+
+  pages.data.products.edges.forEach(edge => {
+    createPage({
+      path: `/portfolio/${edge.node.uid}`,
+      component: component,
+      context: {
+        uid: edge.node.uid,
+      },
+    });
+  });
+};

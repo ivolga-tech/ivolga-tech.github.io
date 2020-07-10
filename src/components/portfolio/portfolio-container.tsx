4c2001cb9152870daed7898452462e98bffff9d4
somakun <somabaka@gmail.com>
2020-07-10 20:39:32 +0300

    reformat portfolio page

--- src/components/portfolio/portfolio-container.tsx
+++ src/components/portfolio/portfolio-container.tsx
@@ -3,13 +3,19 @@ import { Typography } from "../typography/typography";
 import { Button } from "../button/button";
 import { useLocation } from "@reach/router";
 import { Link } from "gatsby";
+import Card from "../card/card";
 
-const PortfolioContainer = () => {
+type Props = {
+  viewModel: any;
+};
+
+const PortfolioContainer = (props: Props) => {
+  const { viewModel } = props;
+  console.log(viewModel);
   const location = useLocation().pathname;
 
   return (
     <section className="section section--cards">
-
       <div className="section__title">
         <Typography component="h2" resetMargin={true}>
           Проекты
@@ -23,43 +29,42 @@ const PortfolioContainer = () => {
 
       <div className="section__content box">
         <div className="row">
-
-          <div className="row__col col-xs-12 col-sm-6 col-md-4">
-            <Link
-              className="card card--lightgrey card--hover"
-              to="/portfolio/waitre/"
-              data-toggle="link"
-              title="Заголовок"
-            >
-              <div className="card__img">
-                <img src="static/img/content/waitre-logo.png" alt="Waitre" title="Waitre" />
+          {viewModel.map((card: any) => {
+            return (
+              <div className="row__col col-xs-12 col-sm-6 col-md-4">
+                <Card
+                  cardType={"link"}
+                  className="card--hover"
+                  to={`/portfolio/${card.portfolio_card_title.text.toLowerCase()}/`}
+                  title={card.portfolio_card_title.text}
+                >
+                  <div
+                    className="card__img"
+                    style={{ background: `${card.card_background_color}` }}
+                  >
+                    <img src={card.portfolio_card_image.url} alt="Waitre" title="Waitre" />
+                  </div>
+                  <div className="card__content">
+                    <div className="card__title">{card.portfolio_card_title.text}</div>
+                    <div className="card__meta">
+                      <ul className="list list--meta">
+                        <li className="list__item">{card.portfolio_card_meta_tegs.text}</li>
+                      </ul>
+                    </div>
+                    <Typography className="card__text">
+                      {card.portfolio_card_description.text}
+                    </Typography>
+                  </div>
+                </Card>
               </div>
-              <div className="card__content">
-                <div className="card__title">Waitre</div>
-                <div className="card__meta">
-                  <ul className="list list--meta">
-                    <li className="list__item">Облачная система управления ресторанами</li>
-                  </ul>
-                </div>
-                <div className="card__text">
-                  Облачная система автоматизации кафе, баров и ресторанов полного цикла. Создается
-                  при активном участии профессионалов ресторанного бизнеса.
-                </div>
-              </div>
-            </Link>
-          </div>
-
+            );
+          })}
         </div>
 
-        <a
-          className="btn btn--mobile btn--stroke btn--stroke-yellow"
-          href="/source-files/portfolio.html"
-        >
+        <Link className="btn btn--mobile btn--stroke btn--stroke-yellow" to="/portfolio/">
           <span>Смотреть все проекты</span>
-          {/*<svg className="icon icon--forward" width="19px" height="16px">*/}
-          {/*  <use xlink:href="#icon-forward"></use>*/}
-          {/*</svg>*/}
-        </a>
+          <img src={require("../../assets/img/minified-svg/icon-forward.svg")} alt="arrow" />
+        </Link>
       </div>
     </section>
   );
--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,15 +1,45 @@
 import React from "react";
 import Layout from "../components/layout/layout";
 import SEO from "../components/seo";
-import PortfolioContainer from "../components/portfolio/portfolio-container"
+import PortfolioContainer from "../components/portfolio/portfolio-container";
+import { graphql } from "gatsby";
 
-const Portfolio = () => {
+const Portfolio = (props: any) => {
   return (
     <Layout>
       <SEO title="portfolio" />
-      <PortfolioContainer />
+      <PortfolioContainer viewModel={props.data.portfolio.edges[0].node.data.portfolio_field} />
     </Layout>
   );
 };
 
 export default Portfolio;
+
+export const PortfolioPageQuery = graphql`
+  query PortfolioPage {
+    portfolio: allPrismicHomepage {
+      edges {
+        node {
+          data {
+            portfolio_field {
+              card_background_color
+              portfolio_card_title {
+                text
+              }
+              portfolio_card_meta_tegs {
+                text
+              }
+              portfolio_card_description {
+                text
+              }
+              portfolio_card_image {
+                url
+                alt
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;

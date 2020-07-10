20510dee72240ffe3988fdfd2a81faf93b41155b
somakun <somabaka@gmail.com>
2020-07-10 22:20:55 +0300

    added default card

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./card.css";
 import { Link } from "gatsby";
-import { joinCssClasses } from "../../utils/utils"
+import { joinCssClasses } from "../../utils/utils";
 
 type Props = {
   children: React.ReactNode;
-  cardType: "form" | "form-modal" | "link";
+  cardType?: "form" | "form-modal" | "link";
   className?: string;
   to?: string;
   title?: string;
@@ -20,12 +20,17 @@ const Card = ({ children, cardType, className, to, title }: Props): JSX.Element 
         return <div className="card card--form card--modal">{children}</div>;
       case cardType === "link":
         return (
-          <Link to={to ? to : ""} className={joinCssClasses("card", className)} data-toggle="link" title={title}>
+          <Link
+            to={to ? to : ""}
+            className={joinCssClasses("card", className)}
+            data-toggle="link"
+            title={title}
+          >
             {children}
           </Link>
         );
     }
-    return <div className="card">{children}</div>;
+    return <div className={joinCssClasses("card", className)}>{children}</div>;
   };
 
   return renderCardType();

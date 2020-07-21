593e1312ef9517d0218332894f5fc9a8ac0a5336
somakun <somabaka@gmail.com>
2020-07-21 15:50:47 +0300

    Added form submission

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./form.css";
 import { joinCssClasses } from "../../utils/utils";
 
@@ -8,31 +8,59 @@ type Props = {
 
 const Form = (props: Props) => {
   const { modal = false } = props;
+  const [loading, setLoading] = useState(false);
+  const [formBtn, setFormBtn] = useState("Отправить");
+
+  async function loginHandler(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const data = new FormData(form);
+    const req = new XMLHttpRequest();
+    setLoading(true);
+    if (form) {
+      req.open(form.method, form.action);
+      req.setRequestHeader("Accept", "application/json");
+      req.onreadystatechange = () => {
+        if (req.readyState !== XMLHttpRequest.DONE) return;
+        if (req.status === 200) {
+          form.reset();
+          setLoading(false);
+          setFormBtn("Отправленно");
+        } else {
+          setFormBtn("Ошибка отправки");
+          setLoading(false);
+          console.log(req.status);
+        }
+      };
+      req.send(data);
+    }
+  }
 
   return (
     <form
       className={joinCssClasses("form form--contact-us", modal ? "modal__form" : "")}
-      action="/"
+      onSubmit={loginHandler}
+      action="https://formspree.io/xqkywzrv"
       method="POST"
     >
       <div className="form__title">Свяжитесь с нами</div>
       <div className="form__content">
         <div className="form__group">
           <label className="form__label">Как вас зовут ?</label>
-          <input id="modal-name" type="text" name="name" required />
+          <input type="text" name="name" required />
         </div>
         <div className="form__group">
           <label className="form__label">Телефон или email для связи</label>
-          <input id="modal-contact" type="text" name="contact" />
+          <input type="text" name="contact" />
         </div>
         <div className="form__group">
           <label className="form__label">Чем мы можем вам помочь ?</label>
-          <textarea id="modal-message" name="message" cols={30} rows={3} />
+          <textarea name="message" cols={30} rows={3} />
         </div>
       </div>
       <div className="form__footer">
-        <button className="btn btn--lg btn--yellow" id="modal-send-message" type="submit">
-          Отправить
+        <button className="btn btn--lg btn--yellow" type="submit" disabled={loading}>
+          {formBtn}
         </button>
       </div>
     </form>
